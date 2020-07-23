f978598ec07b42fc33604a6ff149f5879d050e5f
Mytea <38759537+Mytea-suzukasu123@users.noreply.github.com>
2020-07-23 18:29:19 +0900

    2007231829

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>基本的なPHPの書き方</title>
+    </head>
+    <body>
+
+        <?php
+            echo "HELLO WORLD";
+        ?>
+
+     </body>
+</html>
